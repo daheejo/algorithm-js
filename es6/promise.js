89353682bc014868eb3904977b00a69b574d3791
Dahee Jo <happyj6528@gmail.com>
2022-02-08 02:40:54 +0900

    resolve and reject

--- es6/promise.js
+++ es6/promise.js
@@ -9,3 +9,13 @@ const makeServerRequest = new Promise((resolve, reject) => {
 
 });
 
+const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer represents a response from a server
+    let responseFromServer;
+      
+    if(responseFromServer) {
+      resolve("We got the data")
+    } else {  
+      reject("Data not received")
+    }
+  });
