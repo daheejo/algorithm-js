d7efc85b305719eef2fbf612fce5c3452833c78c
Dahee Jo <happyj6528@gmail.com>
2022-02-20 16:29:02 +0900

    Create revesrseAstring.js

---
+++ basicAlgorithm/revesrseAstring.js
@@ -0,0 +1,9 @@
+function reverseString(str) {
+    let reversed ="";
+    for (let i=str.length-1; i >=0;i--) {
+      reversed += str[i]
+    }
+    return reversed;
+  }
+  
+  reverseString("hello");
