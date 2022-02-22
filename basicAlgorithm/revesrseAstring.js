4a13de185a381d949846dd1fef96a6eda11c6b9d
Dahee Jo <happyj6528@gmail.com>
2022-02-22 21:22:18 +0900

    2번째 방법 업데이트

--- basicAlgorithm/revesrseAstring.js
+++ basicAlgorithm/revesrseAstring.js
@@ -7,3 +7,14 @@ function reverseString(str) {
   }
   
   reverseString("hello");
+
+  function reverse(str){
+
+    var reverStr = "";
+    for (let i = str.length-1;i>=0;i--){
+        reverStr = reverStr+str.charAt(i);
+    }
+    return reverStr;
+}
+
+console.log(reverse("Nice to meet you"));
