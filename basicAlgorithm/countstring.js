5d0f9a498ea857a0cebaa73d18bf80280ac99bca
Dahee Jo <happyj6528@gmail.com>
2022-03-08 14:32:54 +0900

    Update countstring.js

--- basicAlgorithm/countstring.js
+++ basicAlgorithm/countstring.js
@@ -7,3 +7,24 @@
 // 안 촉촉한 초코칩 나라에 살던 안 촉촉한 초코칩이 촉촉한 초코칩 나라의 촉촉한 초코칩을 보고 촉촉한 초코칩이 되고 싶어서 촉촉한 초코칩 나라에 갔는데 촉촉한 초코칩 나라의 문지기가 "넌 촉촉한 초코칩이 아니고 안 촉촉한 초코칩이니까 안 촉촉한 초코칩 나라에서 살아"라고 해서 안 촉촉한 초코칩은 촉촉한 초코칩이 되는 것을 포기하고 안 촉촉한 초코칩 나라로 돌아갔다네
 // 출력 예시 2
 // 13
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+rl.on("line", function (line) {
+    let arr = [];
+    arr.push(line);
+    if  arr.match(/촉촉한 초코칩/) == false {
+        console.log('no match');
+    } else {
+        console.log(str.match(regex).length);
+    }
+}).on("close", function () {
+    
+    process.exit();
+});
