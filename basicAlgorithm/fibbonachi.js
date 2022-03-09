2146b83c2bfce5695a0333fe4fc6b1a60ca8269d
Dahee Jo <happyj6528@gmail.com>
2022-03-09 13:32:58 +0900

    Update fibbonachi.js

--- basicAlgorithm/fibbonachi.js
+++ basicAlgorithm/fibbonachi.js
@@ -29,3 +29,38 @@ rl.on("line", function (line) {
 // 출력 예시
 // [0, 1, 1, 2, 3] 
 // # 다음 항은 5인데 5미만이므로 해당되지 않음
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+var n;
+
+rl.on("line", function (line) {
+  n = parseInt(line);
+  rl.close();
+}).on("close", function () {
+
+
+
+  var fibo = [];
+  
+  if (n > 2) {
+    fibo.push(0);
+    fibo.push(1);
+  } else if (n == 1) {
+    fibo.push(0);
+  }
+  
+  for (var i = 2; i < n; i++) {
+    if (fibo[i - 1] + fibo[i - 2] < n) {
+      fibo.push(fibo[i - 1] + fibo[i - 2]);
+    } else {
+      break;
+    }
+  }
+  console.log(fibo);
+});
