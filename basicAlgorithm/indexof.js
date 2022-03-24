e4d141bf9fc04d414ae16e067b46445b45c03ac4
Dahee Jo <happyj6528@gmail.com>
2022-03-24 22:59:52 +0900

    Update indexof.js

--- basicAlgorithm/indexof.js
+++ basicAlgorithm/indexof.js
@@ -6,3 +6,11 @@
 // The arguments ["hello", "hey"] should return false because the string hello does not contain a y.
 
 // Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien.
+function mutation(arr) {
+    let test = arr[1].toLowerCase();
+    let target = arr[0].toLowerCase();
+    for (let i = 0; i < test.length; i++) {
+      if (target.indexOf(test[i]) < 0) return false;
+    }
+    return true;
+  }
