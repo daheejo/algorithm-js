592e51438250c24b4ac78058a382dbf17762fd9b
Dahee Jo <happyj6528@gmail.com>
2022-03-27 22:32:31 +0900

    writing

---
+++ basicAlgorithm/chunkymonkey.js
@@ -0,0 +1,10 @@
+function chunkArrayInGroups(arr, size) {
+    var newarr = [];
+    for (let i =0;i<arr.length;i++){
+      arr.slice(i, size)
+      newarr.push
+      
+    }
+  }
+  
+  chunkArrayInGroups(["a", "b", "c", "d"], 2);
