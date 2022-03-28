d7a44db4e5b34da3da7dc1da06b27a755cb13631
Dahee Jo <happyj6528@gmail.com>
2022-03-29 00:58:35 +0900

    another way to solve

--- basicAlgorithm/chunkymonkey.js
+++ basicAlgorithm/chunkymonkey.js
@@ -7,3 +7,14 @@ function chunkArrayInGroups(arr, size) {
   }
   
   chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
+function chunkArrayInGroups2(arr.size) {
+    let newArr=[];
+    let i = 0;
+
+    while (i<arr.length) {
+        newArr.push(arr.slice(i,i+size));
+        i+=size;
+    }
+    return newArr;
+} //another way
