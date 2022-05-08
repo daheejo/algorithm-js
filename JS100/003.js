8c2e7fd7f2d6a47300f6b43efa31dd3362322fee
Dahee Jo <happyj6528@gmail.com>
2022-05-09 01:00:09 +0900

    220509 풀이완료

---
+++ JS100/003.js
@@ -0,0 +1,10 @@
+var arr = [100, 200, 300];
+console.log(typeof(arr));
+
+// 다음 출력 값으로 올바른 것은?
+
+// 1)  undefined
+// 2)  string
+// 3)  number
+// 4)  object 정답
+
---
+++ JS100/005.js
@@ -0,0 +1,9 @@
+var a = 10;
+var b = 2;
+
+for(var i=1; i<5; i+=2){
+    a += i;
+}
+
+console.log(a+b);
+//a+b = 10+1+3 + 2 = 16
