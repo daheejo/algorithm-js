92768b0c2ff7544611e064c0e677ed0e22aa157c
Dahee Jo <happyj6528@gmail.com>
2022-05-10 20:35:08 +0900

    220510 풀이완료

---
+++ JS100/009.js
@@ -0,0 +1,17 @@
+
+var year = '2019';
+var month = '04';
+var day = '26';
+var hour = '11';
+var minute = '34';
+var second = '27';
+
+//나의 처음 답
+// var result = [year, month, day, hour, minute, second].concat().join('/');
+var result = year.concat('/', month, '/', day, ' ', hour, ':', minute, ':', second);
+
+console.log(result);
+
+
+// 출력
+// 2019/04/26 11:34:27
