9866c78ea9b22157fdf59dc7249e604cb16d67a3
Dahee Jo <happyj6528@gmail.com>
2022-05-15 21:31:58 +0900

    220515 풀이완료

--- JS100/010.js
+++ JS100/010.js
@@ -9,5 +9,16 @@
 // *********
 
 function makeTree(num){
-    
+    for (let i =1;i<=num;i++){
+        let tree="";
+        for (let j=1;j<=num-i;j++){//공백
+            tree=tree+" ";
+        }
+        for (let k=1;k<=i*2-1;k++){//별추가
+            tree=tree+"*"
+        }
+        console.log(tree)
+    }
 }
+
+makeTree(5)
