5e55d0097764f998083c562efafa34248f95124a
Dahee Jo <happyj6528@gmail.com>
2022-05-19 01:01:07 +0900

    220519 풀이완료

---
+++ JS100/012.js
@@ -0,0 +1,20 @@
+class Wizard {
+    constructor(health, mana, armor){
+        this.health=health
+        this.mana=mana
+        this.armor=armor
+    }
+
+    attack(){
+        console.log('파이어볼')
+    }
+}
+
+const x = new Wizard(545, 210, 10);
+console.log(x.health, x.mana, x.armor);
+x.attack();
+
+
+// 출력
+// 545 210 10
+// 파이어볼
